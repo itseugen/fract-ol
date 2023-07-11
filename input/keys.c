003fc662df7653e045ff7ed0e0b92bcf6a53336c
eweiberl <eweiberl@student.42heilbronn.de>
2023-07-11 16:02:38 +0200

    norm clean (revert if issues)

--- input/keys.c
+++ input/keys.c
@@ -6,7 +6,7 @@
 /*   By: eweiberl <eweiberl@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 10:34:05 by eweiberl          #+#    #+#             */
-/*   Updated: 2023/07/11 15:45:29 by eweiberl         ###   ########.fr       */
+/*   Updated: 2023/07/11 16:02:25 by eweiberl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,74 +42,66 @@ static int	close_window(void *param)
 {
 	t_window	*window;
 
-	/*Add frees if needed*/
 	window = (t_window *)param;
+	exit(0);
+}
+
+/*
 	// mlx_delete_image(window->mlx, window->img);
 	// mlx_close_window(window->mlx);
 	// mlx_terminate(window->mlx);
-	exit(0);
-}
+*/
 
 static void	zoomdetec(double xdelta, double ydelta, void *param)
 {
 	t_params	*params;
-	t_window	*window;
 	int32_t		x;
 	int32_t		y;
 
-	window = &((t_fractol *)param)->window;
 	params = &((t_fractol *)param)->params;
 	(void)xdelta;
 	if (ydelta > 0)
 	{
 		params->zoom = params->zoom + 0.05;
-		params->zoomdif = 1.05;
 		params->zoomin++;
 	}
 	else if (ydelta < 0)
 	{
 		params->zoom = params->zoom - 0.05;
-		params->zoomdif = 0.95;
 		params->zoomout++;
 	}
-
-	double test2;
-
-	test2 = pow(1.05, params->zoomin) * pow(0.95, params->zoomout);
-	params->xmin = -2 / test2;
-	params->xmax = 2 / test2;
-	params->ymin = -2 / test2;
-	params->ymax = 2 / test2;
-	mlx_get_mouse_pos(window->mlx, &x, &y);
+	params->xmin = -2 / pow(1.05, params->zoomin) * pow(0.95, params->zoomout);
+	params->xmax = 2 / pow(1.05, params->zoomin) * pow(0.95, params->zoomout);
+	params->ymin = -2 / pow(1.05, params->zoomin) * pow(0.95, params->zoomout);
+	params->ymax = 2 / pow(1.05, params->zoomin) * pow(0.95, params->zoomout);
+	mlx_get_mouse_pos(((t_fractol *)param)->window.mlx, &x, &y);
 	if (x >= 0 && y >= 0 && x <= WIDTH && y <= HEIGHT && params->zo_mouse == 0)
 		zoom_on_cursor(params, x, y);
-	printf("Current Zoom value: %LF\n", params->zoom);
-	printf("Mouse Position: (%d, %d)\n", x, y);
-	printf("xmin: %LF,\nxmax: %LF\nymin: %LF\n ymax:%LF\n", params->xmin, params->xmax, params->ymin, params->ymax);
-	choose_set(*window, *params);
+	choose_set(((t_fractol *)param)->window, *params);
 }
+
 /*
 void mlx_get_mouse_pos(mlx_t* mlx, int32_t* x, int32_t* y);
+	printf("Current Zoom value: %LF\n", params->zoom);
+	printf("Mouse Position: (%d, %d)\n", x, y);
+	printf("xmin: %LF,\nxmax: %LF\nymin: %LF\n ymax:%LF\n",
+		params->xmin, params->xmax, params->ymin, params->ymax);
 */
 
 static void	zoom_on_cursor(t_params *params, int x, int y)
 {
 	long double	xrange;
 	long double	xmovepp;
-	long double	xmid;
 	long double	yrange;
 	long double	ymovepp;
-	long double	ymid;
 
-	xmid = WIDTH / 2;
 	xrange = (params->xmax + params->xmove) - (params->xmin + params->xmove);
 	xmovepp = xrange / WIDTH;
-	ymid = HEIGHT / 2;
 	yrange = (params->ymax + params->xmove) - (params->ymin + params->xmove);
 	ymovepp = yrange / HEIGHT;
 
-	params->xmove = params->xmove + (xmovepp * (x - xmid));
-	params->ymove = params->ymove + (ymovepp * (y - ymid));
+	params->xmove = params->xmove + (xmovepp * (x - (WIDTH / 2)));
+	params->ymove = params->ymove + (ymovepp * (y - (HEIGHT / 2)));
 
 	params->xmin = params->xmin + params->xmove;
 	params->xmax = params->xmax + params->xmove;
